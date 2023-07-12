faba1fb54fed5c2e75748bb2eed76f87363c6aed
jujusoi <jalxmcdonald@hotmail.com>
2023-07-12 19:00:55 +0930

    added save message whenever u click save button. additional HTML changes and minor CSS changes. got rid of yucky comments. also added a clear button for textareas bcoz i can

--- Develop/script.js
+++ Develop/script.js
@@ -44,8 +44,10 @@ function setDivColors(time, div) {
 
 $("main").on("click", 'button', function() {
   var mainDiv = $("#everything-div");
+  var saveText = $("#save-text");
     if (mainDiv) {
       window.localStorage.setItem("Information", mainDiv.html());
+      saveText.html('Saved schedule to <span id="localStorageSpan">localStorage✔️</span>');
     }
   });
 
@@ -88,27 +90,12 @@ init();
 call();
 setTime();
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+var clearAllButton = $("#clear-all");
+clearAllButton.on("click", function() {
+  var mainDiv = $("#everything-div");
+  var allTextAreas = $("textarea");
+  var saveText = $("#save-text");
+  allTextAreas.text("");
+  window.localStorage.setItem("Information", mainDiv.html());
+  saveText.html('Removed all schedules from <span id="localStorageSpan">localStorage</span>');
+})
